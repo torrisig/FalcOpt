5de49c0708e722d8a41ebe02e12cf323996511d3
Tommaso Robbiani <tommasro@student.ethz.ch>
2017-05-11 14:12:33 +0200

    improved subexpressions initialization

--- +falcopt/fcn2struct.m
+++ +falcopt/fcn2struct.m
@@ -13,6 +13,26 @@
 % - info: .structure : the structure of the matrix
 %         .flops: the flops of the function
 %
+% Copyright (c) 2017 Tommaso Robbiani <tommasro@student.ethz.ch>
+%           
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
 function [data, info] = fcn2struct(varargin)
     p = inputParser;
     p.addRequired('function');
@@ -25,6 +45,8 @@ function [data, info] = fcn2struct(varargin)
 
     data = [];
     subr = {};
+    subr_ind  = [];
+    subr_name = 'sigma_subr'; % internal variables (does not influence outputs)
     info.flops.add = 0;
     info.flops.mul = 0;
     info.flops.div= 0;
@@ -50,21 +72,8 @@ function [data, info] = fcn2struct(varargin)
 
 
     if( ~static)
-        % check existence of subexpression in f entries
-        ind = 1;
-        string = 's1';
-        for i=1:10                  % limit to a max of 10 subexpression
-            [f,subr{ind}] = subexpr(f,string);
-            if isempty(subr{ind})
-                subr(ind) = [];
-                break;
-            else
-                subr{ind} = vpa(simplify(subr{ind}));
-                ind = ind +1;
-                string = sprintf('s%i',ind);
-            end
-            
-        end
+        % check existence of subexpression in f
+        [f,subr,subr_ind] = subexpressions(f,'ordered',subr_name);
         
         M = vpa(simplify(f));
     else
@@ -108,8 +117,7 @@ function [data, info] = fcn2struct(varargin)
     info.static = static;
 
     fun = @replaceEsp; % initialize function called by regexprep (to substitute 'pow')
-    
-    
+    fun_2 = @(x)falcopt.internal.num2str(str2double(x),'precision',o.precision); % initialize function regexprep call to convert numbers
     
     % substitute 'pow' in subexpression where possible and save  subexpressions to data
     if length(subr)>0
@@ -127,7 +135,6 @@ function [data, info] = fcn2struct(varargin)
         tmp = regexprep(tmp,'pow\((.*?,\d\.\d)\)','${fun($1)}');
         
         % convert numbers into single/double format
-        fun_2 = @(x)falcopt.internal.num2str(str2double(x),'precision',o.precision);
         tmp = regexprep(tmp,'(\d+\.\d+E[\-\+]*\d+|\d+\.\d+\d|\d\.\d)','${fun_2($1)}');
        
         % save into data
@@ -144,7 +151,6 @@ function [data, info] = fcn2struct(varargin)
         tmp = regexprep(tmp,'pow\((.*?,\d\.\d)\)','${fun($1)}');
         
         % convert numbers into single/double format
-        fun_2 = @(x)falcopt.internal.num2str(str2double(x),'precision',o.precision);
         tmp = regexprep(tmp,'(\d+\.\d+E[\-\+]*\d+|\d+\.\d+\d|\d\.\d)','${fun_2($1)}');
         
         
@@ -166,6 +172,9 @@ function [data, info] = fcn2struct(varargin)
     for j = o.nu:-1:1
         data = strrep(data, char(u(j)), sprintf('u[%i]',j-1));
     end
+    
+    fun_3 = @(x)sprintf('s%i',subr_ind(str2num(x)));
+    data = regexprep(data,sprintf([subr_name '(\\d)']),'${fun_3($1)}');
     %ToDo Tommaso
     for j = 50:-1:1
         data = strrep(data, char(sl(j)), sprintf('sl[%i]',j-1));
@@ -202,6 +211,8 @@ function [data, info] = fcn2struct(varargin)
 end
 
 function res = replaceEsp(c)
+% function called in regexprep
+% substitute pow(.,n) with (.)*(.)*... n-times
 ex = strsplit(char(c),',');
 esponente = str2num(ex{2});
 num = sprintf(['(' char(ex{1}) ')']);
@@ -216,3 +227,84 @@ else
     res = sprintf(['pow(' num ', %i)'],esponente);
 end
 end
+
+function [f,subr,subr_ind] = subexpressions(f,method,sub_name)
+% extract subexpressions from f.
+% if method == 'ordered' try try to ordere subexrpressions initialization based
+% on dependence within eachother.
+
+subr = {};
+subr_ind = [];
+switch method
+    case 'ordered'
+        ind = 1;
+        string = sprintf([sub_name '1']);
+        for i=1:10                  % limit to a max of 10 subexpression
+            [f,sigma] = subexpr(f,string);
+            if isempty(sigma)
+                % no more subexpr found
+                break;
+            else
+                sigma = vpa(simplify(sigma));
+                for k=1:length(subr) % check if sigma is subexpression in other subr
+                    if strfind(char(subr{k}),char(sigma))
+                        % The idea is to place sigma before the existing subexpression in subr if they contain sigma
+                        % (take index position k) and after the subexpression on which sigma depends (take inedex values k2)
+                        k2 = regexp(char(sigma),sprintf([sub_name '(\\d)']),'tokens');
+                        if ~isempty(k2)
+                            k2 = min(str2num(cell2mat(k2{:})));
+                        end
+                       
+                        % substitute the subexp sigma in other expression in subr
+                        subr(k:end) = cellfun(@(x)subs(x,sigma,string),subr(k:end),'UniformOutput',false);
+                       
+                        % updated the indices positions in subr_ind
+                        if isempty(k2)||k>subr_ind(k2)
+                            subr_ind(subr_ind(:)>= k ) = subr_ind(subr_ind(:)>= k ) +1 ;
+                            subr_ind(end+1)=k;
+                        else
+                            pos = subr_ind(k2)+1;
+                            subr_ind(subr_ind(:)>= pos ) = subr_ind(subr_ind(:)>= pos) +1 ;
+                            subr_ind(end+1)=pos;
+                        end
+                       
+                        break;
+                    end
+                end
+               
+                % place sigma at the end
+                subr = [subr,{sigma}];
+                if length(subr_ind) ~= length(subr)||isempty(subr)
+                    subr_ind = [subr_ind,ind];
+                end
+               
+                % update sbexpression name
+                ind = ind +1;
+                string = sprintf([sub_name '%i'],ind);
+            end
+        end
+       
+        % order subexpr based on subr_ind
+        tmp = subr;
+        for k = 1:length(subr)
+            subr{k} = tmp{find(subr_ind==k)};
+        end
+       
+    otherwise
+        ind = 1;
+        string = sprintf([sub_name '1']);
+        for i=1:10                  % limit to a max of 10 subexpression
+            [f,subr{ind}] = subexpr(f,string);
+            if isempty(subr{ind})
+                subr(ind) = [];
+                break;
+            else
+                subr{ind} = vpa(simplify(subr{ind}));
+                ind = ind +1;
+                string = sprintf([sub_name '%i'],ind);
+            end
+        end
+        subr_ind = [1:length(subr)];
+end 
+end
+
