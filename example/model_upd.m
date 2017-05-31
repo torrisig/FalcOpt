99c63ae9870f2073e87d15e0debdf08d54c7b27f
Tommaso Robbiani <tommasro@student.ethz.ch>
2017-05-31 12:23:33 +0200

    comments on parameters

--- example/model_upd.m
+++ example/model_upd.m
@@ -1,16 +1,16 @@
 function xp = model_upd (x,u,Ts)
 
-Rr = 1.1747;
-Lrbar = 0.210;
-Lhbar = 0.2011;
-b = Rr*Lhbar/Lrbar;
-a = -Rr/Lrbar;
-J = 0.0053;
-Tload = 0.3;
-k = 1e-2;
+Rr = 1.1747;                    % rotor resistance
+Lrbar = 0.210;                  % rotor inductance
+Lhbar = 0.2011;                 % magnetization inductance
+J = 0.0053;                     % motor inertia
+Tload = 0.3;                    % load torque
+k = 1e-2;                       % friction coefficient
+a = -Rr/Lrbar;   
+b1 = Rr*Lhbar/Lrbar;
+b2 = Lhbar/Lrbar;
 
-
-xp = [(1+Ts*a)*x(1) + Ts*b*u(1);...
-    (1-Ts*k/J)*x(2) + Ts/J*(Lhbar/Lrbar*x(1)*u(2) -  Tload)];
+xp = [ x(1) + ( a*x(1) + b1*u(1))*Ts;...
+       x(2) + 1/J*( b2*x(1)*u(2) - Tload - k*x(2))*Ts];
 
 end
